a25a6abcfb91980fd50de629bdba0458fc00b533
Sarah Dawson <knittingarch@gmail.com>
2016-04-30 00:14:05 -0400

    changed number range in input request

--- Exercises/5-2.py
+++ Exercises/5-2.py
@@ -6,7 +6,7 @@ by Sarah Dawson
 
 def main():
 	letter_grade = ['F', 'F', 'D', 'C', 'B', 'A']
-	num_grade = eval(raw_input("Please enter your numerical grade (1-5): "))
+	num_grade = eval(raw_input("Please enter your numerical grade (0-5): "))
 	print "You earned a ", letter_grade[num_grade], "on that quiz."
 
 main()	
