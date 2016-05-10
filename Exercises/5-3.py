ea3911018f1126642c23b14f6ed9850de8c0f661
Sarah Dawson <knittingarch@gmail.com>
2016-05-09 23:10:07 -0400

    convert numerical grades to letter; list sequencing and if statements

--- Exercises/5-3.py
+++ Exercises/5-3.py
@@ -1,21 +1,28 @@
 '''
 A program that accepts an exam score between 0-100 and asigns a letter grade based on 10-pt scale (90-100 = A, etc)
+This program uses list sequencing, although a simple if/elif/else statement with integers would work as well.
 by Sarah Dawson
 '''
 
 def main():
 
-	grade = eval(raw_input('Please enter your grade here: '))
+	print 'This program will convert your numerical grade into a letter grade. \n'
 
-	if 90 <= grade <=100:
-		print 'A'
-	elif 80 < grade < 90:
-		print 'B'
-	elif 70 < grade < 80:
-		print 'C'
-	elif 60 < grade < 70:
-		print 'D'
+	grade = eval(raw_input('Please enter your grade here: '))
+	numbers = []
+
+	for i in range(101):
+		numbers.append(i)
+
+	if grade >= numbers[90]:
+		print 'You earned an A.'
+	elif numbers[80] < grade < numbers[90]:
+		print 'You earned an B.'
+	elif numbers[70] < grade < numbers[80]:
+		print 'You earned an C.'
+	elif numbers[60] < grade < numbers[70]:
+		print 'You earned an D.'
 	else:
-		print 'F'
+		print 'You earned an F'
 
 main()
