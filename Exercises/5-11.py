2974dd4d7b6adce4ff32b665929e92125c4d66de
Sarah Dawson <knittingarch@gmail.com>
2016-05-25 11:12:50 -0400

    string formatting; updated via format()

--- Exercises/5-11.py
+++ Exercises/5-11.py
@@ -7,16 +7,16 @@ def main():
 	y = eval(raw_input("Enter a number between 0 and 1: "))
 
 	# Create table labels
-	print '\n{0:^10} {1:^15.2f} {2:^15.2f}'.format("index", x, y)
+	print '\n{0:^5} {1:^15.2f} {2:^15.2f}'.format("index", x, y)
 
 	# Create horizontal rule
-	print "-"*41	
+	print "-"*33
 
 	# Iterate through items and apply formula
 	for i in range(15):
 		x = 3.9 * x * (1 - x)
 		y = 3.9 * y * (1 - y)
 		# Print out the values in a formatted table
-		print '{0:^10} {1:^15.6f} {2:^15.6f}'.format(i, x, y)
+		print '{0:^5} {1:^15.6f} {2:^15.6f}'.format(i, x, y)
 
 main()
